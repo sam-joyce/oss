42321a2aa9b8a08b52f2cda3d40cdb7d576879b8
Sam Joyce <samjoycesamjoyce@Sams-MBP.home>
2020-04-21 12:33:44 +0100

    favicon added - map added to contact page - phone number now a call link - empty components added for Bar/Sheet Pages

--- src/Components/ContactPage/ContactPage.jsx
+++ src/Components/ContactPage/ContactPage.jsx
@@ -9,7 +9,6 @@ class ContactPage extends Component {
         <header className={styles.img_box}>
           <div className={styles.color_overlay}></div>
           <h1>Contact Us</h1>
-          {/* <h2>We'd love to hear from you.</h2> */}
         </header>
 
         <section className={styles.container}>
@@ -44,7 +43,7 @@ class ContactPage extends Component {
                   <h5>Message *</h5>
                   <input type="text" name="message" id={styles.message} />
                 </div>
-                <button>SUBMIT</button>
+                <button id={styles.submit}>SUBMIT</button>
               </div>
             </div>
             <div></div>
@@ -53,18 +52,21 @@ class ContactPage extends Component {
                 <p>
                   Offshore Stainless Supplies Ltd
                   <br />
-                  Toll Point <br/>
-                  Lichfield Road <br/>
-                  Brownhills <br/>
-                  Walsall <br/>
-                  West Midlands <br/>
-                  WS8 6JZ <br/><br/>
-                  T: +44 (0)1922 414003 <br/>
-                  F: +44 (0)1922 414606
+                  Toll Point <br />
+                  Lichfield Road <br />
+                  Brownhills <br />
+                  Walsall <br />
+                  West Midlands <br />
+                  WS8 6JZ <br /><br />
+                  T :<a href="tel: 01922414003">+44 (0)1922 414003</a> <br />
+                  F :<a href="tel: 01922414606">+44 (0)1922 414606</a>
                 </p>
               </div>
               <div id={styles.breaker}><hr /></div>
-              <div id={styles.map}></div>
+              <div id={styles.map}>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d38727.90248578462!2d-2.005216007642915!3d52.65105593874914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48709f9881ea4ff9%3A0xf3299b5b372f7b8b!2sOffshore%20Stainless%20Supplies%20Ltd!5e0!3m2!1sen!2suk!4v1587463536287!5m2!1sen!2suk">
+                </iframe>
+              </div>
             </div>
           </div>
         </section>
