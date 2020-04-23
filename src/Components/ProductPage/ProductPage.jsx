c09acb37668aaa777237a6d130b68fc79168b50c
Sam Joyce <samjoycesamjoyce@Sams-MBP.home>
2020-04-23 17:46:21 +0100

    ProductPage outline done, components made to slot into relevant sections - Bar/Sheet Routes possibly to be added next

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import styles from "./ProductPage.module.scss";
 
 class ProductPage extends Component {
-  state = {  }
+  state = {}
   render() { 
     return (
-      <p className={styles.para}>This is the Product Page</p>
+      <>
+        <div className={styles.container}>
+          <div className={styles.inner_container}>
+            <section className={styles.column1}>
+              <article className={styles.video}>
+                Video
+              </article>
+              <article className={styles.noticeBoard}>
+                Noticeboard
+              </article>
+            </section>
+            <section className={styles.column2}>
+              <article className={styles.productList}>
+                Product List
+              </article>
+            </section>
+            <section className={styles.column3}>
+              <article>Approvals</article>
+              <article>Tech Datasheets</article>
+              <article>Product Literature</article>
+              <article>Legal Information</article>
+            </section>
+          </div>
+        </div>
+      </>
     );
   }
 }
