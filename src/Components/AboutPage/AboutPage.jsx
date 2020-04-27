4361ce915838bdf1c982c3b43eaab5d8db2ce058
Sam Joyce <samjoycesamjoyce@Sams-MBP.home>
2020-04-27 11:09:22 +0100

    about page outline completed, just waiting for actual text - disclaimer page completed - borders removed from all pages - contact page fixed to correct height

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import styles from "./AboutPage.module.scss";
+
+import about from "../Static/Images/about.jpg";
+import team from "../Static/Images/team.jpg";
+
 
 class AboutPage extends Component {
-  state = {  }
-  render() { 
-    return (  
-      <h1>This is the About Page</h1>
+  state = {}
+  render() {
+    return (
+      <section className={styles.container}>
+        <article>
+          <h2>About Us</h2> <br/>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis iste nihil velit! Illum ipsa rem quidem officiis esse deleniti in voluptatibus officia velit! Quod eos, quidem quibusdam error possimus perferendis? <br/> <br/> Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quod molestias incidunt laudantium, ipsa atque repudiandae soluta illo consequatur nesciunt iusto tempore. Provident doloremque dolor ipsam voluptas amet alias hic.</p>
+        </article>
+        <article>
+          <img src={about} alt=""/>
+        </article>
+        <article>
+          <img src={team} alt=""/>
+        </article>
+        <article>
+        <h2>Why Offshore?</h2> <br/>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat laboriosam modi, sunt ipsam illum blanditiis, officia reiciendis necessitatibus natus debitis, quis nisi eligendi. Animi commodi amet eum quasi harum nihil. <br/> <br/> Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia, iure consequatur itaque porro voluptatum labore soluta. Reprehenderit ipsum, a ducimus, explicabo, voluptas animi fugit minima optio cupiditate harum nisi.</p>
+        </article>
+      </section>
     );
   }
 }
- 
+
 export default AboutPage;
--- src/Components/DisclaimerPage/DisclaimerPage.jsx
+++ src/Components/DisclaimerPage/DisclaimerPage.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import styles from "./DisclaimerPage.module.scss";
 
 class DisclaimerPage extends Component {
-  state = {  }
-  render() { 
+  state = {}
+  render() {
     return (
-      <h1>This is the Disclaimer Page</h1>
+      <div className={styles.disclaimer_container}>
+      <section className={styles.disclaimer_inner}>
+        <h2>Disclaimer - All Products Supplied</h2>
+        <br/>
+        <h6>This information for all products is based on our present knowledge and is given in good faith.
+        <br/> <br/>
+        However, no liability will be accepted by Offshore Stainless Supplies Ltd in respect of any action taken by any third party in reliance thereon, as the products detailed may be used for a wide variety of purposes and Offshore Stainless Supplies Ltd has no control over their use.
+        <br/> <br/>
+        Offshore Stainless Supplies Ltd specifically excludes all conditions or warranties expressed or implied by statute or otherwise as to dimensions, properties and / or fitness for any particular purpose.Any advice given by Offshore Stainless Supplies Ltd to any third party is given for that party`s assistance only and without liability on the part of Offshore Stainless Supplies Ltd.
+        <br/> <br/>
+        Any contract between Offshore Stainless Supplies Ltd and a customer will be subject to our conditions of sale. The extent of our liabilities to any customer is clearly set out in those conditions; a copy of which is available on request.
+        <br/> <br/>
+        Thank you!
+        </h6>
+      </section>
+      </div>
     );
   }
 }
