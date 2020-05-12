f1faabf9849e087bd11c7f187daf1c31567ab3df
Sam Joyce <samjoycesamjoyce@Sams-MBP.home>
2020-05-12 18:00:31 +0100

    products page styling done after design change - content to slot in next plus images

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -1,54 +1,45 @@
 import React, { Component } from 'react';
 import styles from "./ProductPage.module.scss";
+import {
+  Tabs,
+  Tab
+} from "react-bootstrap";
+// import ResponsivePlayer from "./ResponsivePlayer/ResponsivePlayer";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-import ResponsivePlayer from "./ResponsivePlayer/ResponsivePlayer";
-
-import { FaCheck } from 'react-icons/fa';
-import { FaClipboardList } from 'react-icons/fa';
-import { FaFile } from 'react-icons/fa';
-import { FaHandshake } from 'react-icons/fa';
-import ProductList from './ProductList/ProductList';
+// import { FaCheck } from 'react-icons/fa';
+// import { FaClipboardList } from 'react-icons/fa';
+// import { FaFile } from 'react-icons/fa';
+// import { FaHandshake } from 'react-icons/fa';
+// import ProductList from './ProductList/ProductList';
 
 class ProductPage extends Component {
-  state = {}
   render() {
+    Aos.init();
     return (
       <>
-        <div className={styles.container}>
-          <div className={styles.inner_container}>
-            <section className={styles.column1}>
-              <article className={styles.video}>
-                <ResponsivePlayer />
-              </article>
-              <article className={styles.noticeBoard}>
-                Noticeboard
-              </article>
+        {/* <article className={styles.video}>
+          <ResponsivePlayer />
+        </article> */}
+        <Tabs defaultActiveKey="bar" id="uncontrolled-tab-example">
+          <Tab eventKey="sheet" title="Sheet" className={styles.card_container}>
+            <section className={styles.container}>
+              <article></article>
+              <article></article>
+              <article data-aos="fade-in"></article>
+              <article data-aos="fade-in"></article>
             </section>
-            <section className={styles.column2}>
-              <article className={styles.productList}>
-                <ProductList />
-              </article>
+          </Tab>
+          <Tab eventKey="bar" title="Bar" className={styles.card_container}>
+            <section className={styles.container}>
+              <article></article>
+              <article></article>
+              <article data-aos="fade-in"></article>
+              <article data-aos="fade-in"></article>
             </section>
-            <section className={styles.column3}>
-              <article>
-                <h4>Approvals</h4>
-                <a href=""><FaCheck /></a>
-              </article>
-              <article>
-                <h4>Tech Datasheets</h4>
-                <a href=""><FaClipboardList /></a>
-              </article>
-              <article>
-                <h4>Product Literature</h4>
-                <a href=""><FaFile /></a>
-              </article>
-              <article>
-                <h4>Legal Information</h4>
-                <a href=""><FaHandshake /></a>
-              </article>
-            </section>
-          </div>
-        </div>
+          </Tab>
+        </Tabs>
       </>
     );
   }
